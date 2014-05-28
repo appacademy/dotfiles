ae07b595a9a64c1a0f3a02f826587ceeeaee7c4f
Will Hastings <will@objectdotcreate.net>
2014-05-28 11:38:52 -0700

    Add install script.

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s ~/.dotfiles/bashrc ~/.bashrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/inputrc ~/.inputrc
+ln -s ~/.dotfiles/profile ~/.profile
+ln -s ~/.dotfiles/pryrc ~/.pryrc
+ln -s ~/.dotfiles/rdebugrc ~/.rdebugrc
