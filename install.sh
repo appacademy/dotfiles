553211cb51e9ae2225bf187a528fd23512b18f8a
App Academy Student <student@appacademy.io>
2014-05-29 14:07:29 -0700

    fix fucked gitignore

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/.gitignore ~/.gitignore
+ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/inputrc ~/.inputrc
 ln -s ~/.dotfiles/profile ~/.profile
 ln -s ~/.dotfiles/pryrc ~/.pryrc
