3fd423a64147dccca2d3a3b5427364089deb4029
davidrunger <daverunger@gmail.com>
2015-03-03 10:21:28 -0800

    remove background-changing commands
    they seemed to work on ~1/2 of the mac minis...

--- install.sh
+++ install.sh
@@ -14,6 +14,3 @@ ln -sf ~/.dotfiles/rubocop.yml ~/.rubocop.yml
 ln -sf ~/.dotfiles/sqliterc ~/.sqliterc
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 cp ~/.dotfiles/atom/config.cson ~/.atom/config.cson
-osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/.dotfiles/shortcuts.png"'
-sudo ln -sF ~/.dotfiles/shortcuts.png /System/Library/CoreServices/DefaultDesktop.jpg
-sudo rm -rf /Library/Caches/com.apple.desktop.admin.png
